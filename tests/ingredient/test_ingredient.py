d9aec7bd6f7709cb0f38af3ebf6a7d36afadc1d5
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-08-08 20:00:10 -0300

    primeiro requuisito

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,5 +3,12 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    
-    pass
+    ingredient = Ingredient('tomate')
+    other_ingredient = Ingredient('presunto')
+    assert ingredient.name == 'tomate'
+    assert other_ingredient.restrictions != set()
+    assert repr(ingredient) == "Ingredient('tomate')"
+    assert ingredient.__eq__(other=other_ingredient) is False
+    assert ingredient.__eq__(other=ingredient) is True
+    assert hash(ingredient) != hash(other_ingredient)
+    assert hash(ingredient) == hash(ingredient)
